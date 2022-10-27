575a1a7b0f6a59749bbff2cfc357f3c4cafa004a
swskipper <skipper@southwinds.io>
2022-10-27 10:41:55 +0100

    fix container image build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ PLUGIN_PREFIX="dbman-db-"
 cd plugin/pgsql
 go build -o ${PLUGIN_PREFIX}pgsql
 cd ../..
-mv ./plugin/pgsql/${PLUGIN_PREFIX}pgsql ./${PLUGIN_PREFIX}pgsql
+mv ./plugin/pgsql/${PLUGIN_PREFIX}pgsql ./build/${PLUGIN_PREFIX}pgsql
 go build .
